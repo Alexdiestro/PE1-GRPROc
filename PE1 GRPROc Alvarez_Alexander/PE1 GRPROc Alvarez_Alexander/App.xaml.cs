df18c6d91809bc956aa77d69d03708b84c30d236
Alexander Alvarez <Cristian.ValtierraAlvarez@student.pxl.be>
2024-10-22 09:30:43 +0200

    Add files via upload

---
+++ PE1 GRPROc Alvarez_Alexander/PE1 GRPROc Alvarez_Alexander/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace PE1_GRPROc_Alvarez_Alexander
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
